ef86c352b3532b794ba3bda0ba15c830e893460f
Alexandru Toader <atoader@streamlit.io>
2022-12-09 09:50:36 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit 3!
+# Welcome to Streamlit 1!
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
 forums](https://discuss.streamlit.io).
