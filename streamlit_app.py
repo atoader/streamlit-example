208eb00b57994d0782948f828c9fdc925975480a
Alexandru Toader <atoader@streamlit.io>
2023-04-18 19:12:18 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ import math
 import pandas as pd
 import streamlit as st
 
-st.components.v1.iframe("https://atoader-streamlit-example-streamlit-app-p3r1v1.streamlitapp.com/", width=1000, height=1000, scrolling=False)
+st.components.v1.iframe("https://atoader-streamlit-example-streamlit-app-oo2v2d.streamlit.app/", width=1000, height=1000, scrolling=False)
